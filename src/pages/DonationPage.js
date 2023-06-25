ea6581bbbd0b2dbae393eb314047c88445f45b11
Gabriel Maiate <gvp@vortx.com.br>
2023-06-24 22:40:32 -0300

    att no front

--- src/pages/DonationPage.js
+++ src/pages/DonationPage.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import { TextField, Select, MenuItem, FormControl, InputLabel, Button, Box, Container } from '@mui/material';
 
 function DonationPage() {
+
+  const [person, setPerson] = React.useState('');
+  const [hairAmount, setHairAmount] = React.useState('');
+
+  const handleChangePerson = (event) => {
+    setPerson(event.target.value);
+  };
+
+  const handleChangeHairAmount = (event) => {
+    setHairAmount(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Person: ${person}, Hair Amount: ${hairAmount}`);
+  };
+
   return (
-    <div>
-      <h1>Donation Page</h1>
-    </div>
+    <Container maxWidth="sm">
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <h1>Donation Page</h1>
+        <form onSubmit={handleSubmit}>
+          <FormControl fullWidth variant="outlined" style={{ marginBottom: '20px' }}>
+            <InputLabel id="person-label">Person</InputLabel>
+            <Select
+              labelId="person-label"
+              id="person"
+              value={person}
+              onChange={handleChangePerson}
+              label="Person"
+            >
+              <MenuItem value="Person 1">Person 1</MenuItem>
+              <MenuItem value="Person 2">Person 2</MenuItem>
+              <MenuItem value="Person 3">Person 3</MenuItem>
+              {/* ...additional options... */}
+            </Select>
+          </FormControl>
+          <TextField
+            id="hair-amount"
+            label="Hair Amount to Donate"
+            type="number"
+            variant="outlined"
+            value={hairAmount}
+            onChange={handleChangeHairAmount}
+            fullWidth
+            style={{ marginBottom: '20px' }}
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </form>
+      </Box>
+    </Container>
   );
 }
 
--- src/pages/TrackingPage.js
+++ src/pages/TrackingPage.js
@@ -1,11 +1,88 @@
 import React from 'react';
+import { 
+  Table, 
+  TableBody, 
+  TableCell, 
+  TableContainer, 
+  TableHead, 
+  TableRow, 
+  Paper, 
+  Box, 
+  Collapse, 
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+//  TimelineConnector,
+  TimelineContent,
+  TimelineDot
+} from '@mui/material';
+import { TimelineConnector } from '@mui/lab';
 
-function TrackingPage() {
+
+function TablePage() {
+  const [open, setOpen] = React.useState(false);
+  const [selectedRow, setSelectedRow] = React.useState(null);
+
+  const handleClick = (rowId) => {
+    if (selectedRow === rowId) {
+      setOpen(!open);
+    } else {
+      setSelectedRow(rowId);
+      setOpen(true);
+    }
+  };
+
+  const data = [
+    { id: 1, donor: 'Donor 1', quantity: '2kg', events: ['Event1', 'Event2', 'Event3'] },
+    { id: 2, donor: 'Donor 2', quantity: '3kg', events: ['Event1', 'Event2'] },
+    { id: 3, donor: 'Donor 3', quantity: '1.5kg', events: ['Event1'] },
+  ];
+
   return (
-    <div>
-      <h1>Tracking Page</h1>
-    </div>
+    <Box display="flex" justifyContent="center" m={2}>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>DOADOR</TableCell>
+              <TableCell>QUANTIDADE</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => (
+              <React.Fragment key={row.id}>
+                <TableRow hover onClick={() => handleClick(row.id)}>
+                  <TableCell>{row.id}</TableCell>
+                  <TableCell>{row.donor}</TableCell>
+                  <TableCell>{row.quantity}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open && selectedRow === row.id} timeout="auto" unmountOnExit>
+                      <Box margin={1}>
+                        <Timeline>
+                          {row.events.map((event, index) => (
+                            <TimelineItem key={index}>
+                              <TimelineSeparator>
+                                <TimelineDot />
+                                {index < row.events.length - 1 && <TimelineConnector />}
+                              </TimelineSeparator>
+                              <TimelineContent>{event}</TimelineContent>
+                            </TimelineItem>
+                          ))}
+                        </Timeline>
+                      </Box>
+                    </Collapse>
+                  </TableCell>
+                </TableRow>
+              </React.Fragment>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
 
-export default TrackingPage;
+export default TablePage;
