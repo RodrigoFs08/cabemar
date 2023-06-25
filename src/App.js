7f3cf21a4a87d6454c5d4aa99fd0dec4ff07795d
Rodrigo Ferreira Santos <rfs@tokenizadora.com>
2023-06-25 10:48:48 -0300

    modificações hgomepage

--- src/App.js
+++ src/App.js
@@ -34,76 +34,7 @@ const theme = createTheme({
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [account, setAccount] = useState("");
-  const [entryTokenContract, setEntryTokenContract] = useState(null);
-  const [hasToken, setHasToken] = useState(false);
-  useEffect(() => {
-    connectToMetaMask();
-    initializeEntryTokenContract();
-  }, []);
 
-  const connectToMetaMask = async () => {
-    if (window.ethereum) {
-      try {
-        // Request account access
-        await window.ethereum.request({ method: "eth_requestAccounts" });
-        // Get the selected account
-        const accounts = await window.ethereum.request({
-          method: "eth_accounts",
-        });
-        if (accounts.length > 0) {
-          setAccount(accounts[0]);
-          setLoggedIn(true);
-        }
-      } catch (error) {
-        console.error("Error connecting to MetaMask:", error);
-      }
-    } else {
-      console.error("MetaMask not found.");
-    }
-  };
-
-  const initializeEntryTokenContract = async () => {
-    // Detecta o provedor Ethereum (Metamask, por exemplo)
-    const provider = await detectEthereumProvider();
-
-    if (provider) {
-      // Cria uma nova instância Web3 usando o provedor
-      const web3 = new Web3(provider);
-
-      // Obtém as contas
-      const accounts = await web3.eth.getAccounts();
-
-      // Cria uma instância do contrato
-      const contract = new web3.eth.Contract(
-        EntryTokenABI,
-        "0xBe5A57983911C6a9e0674E122f57fd02B238bD5C"
-      );
-
-      // Chama as funções do contrato e atualiza o estado do componente
-      const balance = await contract.methods.balanceOf(accounts[0]).call();
-      if (balance > 0) {
-        setHasToken(true);
-        console.log("balance aqui", balance);
-      } else {
-        console.log("Tá zerado");
-        //REDIRECIONA PARA SOLICITAR CERTIFICAÇÃO
-      }
-    } else {
-      console.log("Please install MetaMask!");
-    }
-    // try {
-    //   const web3 = new Web3(window.ethereum);
-    //   const entryToken = new web3.eth.Contract(
-    //     EntryTokenABI,
-    //     "0xBe5A57983911C6a9e0674E122f57fd02B238bD5C"
-    //   );
-    //   setEntryTokenContract(entryToken);
-    // } catch (error) {
-    //   console.error("Error initializing EntryToken contract:", error);
-    // }
-  };
 
   const handleSidebarOpen = () => {
     setSidebarOpen(true);
@@ -118,21 +49,19 @@ function App() {
       <Router>
         <Header
           onMenuClick={handleSidebarOpen}
-          loggedIn={loggedIn}
-          account={account}
+
         />
-        {loggedIn && (
-          <Sidebar isOpen={sidebarOpen} onClose={handleSidebarClose} />
-        )}
+
+        <Sidebar isOpen={sidebarOpen} onClose={handleSidebarClose} />
+
         <Routes>
-          <Route path="/home" exact element={<HomePage />} />
+          <Route path="/" exact element={<HomePage />}
+          />
           <Route path="/donation" element={<DonationPage />} />
           <Route path="/tracking" element={<TrackingPage />} />
           <Route path="/certification" element={<CertificationPage />} />
           <Route path="/rewards" element={<RewardsPage />} />
-          {loggedIn && (
-            <Route path="/userCredentials" element={<UserCredentialsPage />} />
-          )}
+
         </Routes>
       </Router>
     </ThemeProvider>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/system/Box";
 import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
+import EntryTokenABI from "../contracts/abi/EntryToken.json";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 function HomePage() {
+  const [account, setAccount] = useState("");
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [severity, setSeverity] = useState("success");
+  const [hasToken, setHasToken] = useState(false);
+// useEffect(() => {
+//     connectToMetaMask();
+//     initializeEntryTokenContract();
+//   }, []);
+    const initializeEntryTokenContract = async () => {
+    // Detecta o provedor Ethereum (Metamask, por exemplo)
+    const provider = await detectEthereumProvider();
+
+    if (provider) {
+      // Cria uma nova instância Web3 usando o provedor
+      const web3 = new Web3(provider);
+
+      // Obtém as contas
+      const accounts = await web3.eth.getAccounts();
+
+      // Cria uma instância do contrato
+      const contract = new web3.eth.Contract(
+        EntryTokenABI,
+        "0xBe5A57983911C6a9e0674E122f57fd02B238bD5C"
+      );
+
+      // Chama as funções do contrato e atualiza o estado do componente
+      const balance = await contract.methods.balanceOf(accounts[0]).call();
+      if (balance > 0) {
+        setHasToken(true);
+        console.log("balance aqui", balance);
+      } else {
+        console.log("Tá zerado");
+        //REDIRECIONA PARA SOLICITAR CERTIFICAÇÃO
+      }
+    } else {
+      console.log("Please install MetaMask!");
+    }
+    
+  };
+
+
+  const connectToMetaMask = async () => {
+    const provider = await detectEthereumProvider();
+
+    if (provider) {
+      try {
+        const accounts = await provider.request({
+          method: "eth_requestAccounts",
+        });
+
+        if (accounts.length > 0) {
+          setAccount(accounts[0]);
+          setMessage("Successfully connected to MetaMask");
+          setSeverity("success");
+          setOpen(true);
+        }
+      } catch (error) {
+        setMessage("Failed to connect to MetaMask");
+        setSeverity("error");
+        setOpen(true);
+      }
+    } else {
+      setMessage("MetaMask not found");
+      setSeverity("error");
+      setOpen(true);
+    }
+  };
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
     <Container>
       <Box
@@ -12,9 +98,24 @@ function HomePage() {
           justifyContent: "center",
           alignItems: "center",
           height: "calc(100vh - 64px)",
-          backgroundColor: "#86CFD5",
         }}
-      ></Box>
+      >
+        {account === "" ? (
+          <Button variant="contained" onClick={connectToMetaMask}>
+            Connect to MetaMask
+          </Button>
+        ) : (
+          <p>Welcome, member {account}</p>
+        )}
+
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <div>
+          <Alert onClose={handleClose} severity={severity}>
+            {message}
+          </Alert>
+          </div>
+        </Snackbar>
+      </Box>
     </Container>
   );
 }
